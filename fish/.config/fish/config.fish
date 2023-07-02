89e5901e011620cb0a31f499a7d361a6111426e7
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2023-07-02 17:06:22 -0500

    feat: add aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,8 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias c='~/.dotfiles/scripts/open_proj.sh'
-
+# Open configurations
 alias c.='cd ~/.dotfiles/ && vim .'
 alias cf='vim ~/.config/fish/config.fish'
 alias cs='vim ~/.config/starship.toml'
@@ -19,10 +18,21 @@ alias dcud='sudo docker compose up -d'
 alias dcd='sudo docker compose down'
 alias dcl='sudo docker compose logs -f'
 
+alias c='~/.dotfiles/scripts/open_proj.sh'
+alias cat='bat'
+alias grep='rg'
+alias l='exa -hl'
+alias ls='exa'
+
 alias nvm='fnm'
+alias rm='echo -e "Woahh, you are using rm instead of r!\nBe careful..." && /usr/sbin/rm -i'
+alias r='trash'
 
 alias h='cd /mnt/c/Users/TestUser/'
 alias home='cd /mnt/c/Users/TestUser/'
+
+alias s='source ~/.config/fish/config.fish'
+alias take 'function __take; mkdir -p $argv; cd $argv; end; __take'
 
 alias vim='/usr/sbin/nvim'
 alias v='/usr/sbin/nvim .'
