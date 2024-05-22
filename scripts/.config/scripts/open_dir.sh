5468a9a83946a4099306c87f24813149e484b986
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2024-05-22 16:37:19 +0200

    feat: improve open dir script

--- scripts/.config/scripts/open_dir.sh
+++ scripts/.config/scripts/open_dir.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-# Define the directories to search in
-directories="$@"
-# directories=("$HOME/dev" "$HOME/work")
-
 # Search for directories using fzf
-selected_dir=$(find ${directories[@]} -maxdepth 1 -type d -not -path '*/.*' 2>/dev/null | fzf)
+directories="$@"
+selected_dir=$(find ${directories[@]} -maxdepth 1 -type d -not -path '*/.*' 2>/dev/null | sort | fzf)
 
 # Check if a directory was selected
 if [[ ! -n "$selected_dir" ]]; then
-    echo "No directory selected"
+    pwd
     exit 1
 fi
 
